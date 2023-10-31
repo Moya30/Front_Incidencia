f474d36fe61238bd8e36b3870f940cdafc27cdc1
cristhianH <cristhian.dbest@gmail.com>
2023-10-30 21:39:17 -0500

    Cmabios rdo usuario api

--- src/pages/DetailsUser.jsx
+++ src/pages/DetailsUser.jsx
@@ -3,8 +3,8 @@ import Navbar from "../components/Navbar/Index";
 import { sidebarToggle } from '../utils/toggler';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFloppyDisk, faPhone } from '@fortawesome/free-solid-svg-icons';
-import {AddUser} from '../libs/AddUser'
-
+import AddUser from '../libs/AddUser'
+import { useNavigate } from "react-router-dom";
 export const DetailsUser = () => {
   
 
@@ -18,7 +18,7 @@ export const DetailsUser = () => {
   const [emailPers, setEmailPers] = useState("");
   const [nombRol, setNombRol] = useState("");
 
-
+  const navigate = useNavigate();
   const handleNombUserChange = (e) => {
     setNombUsua(e.target.value);
   }
@@ -27,13 +27,37 @@ export const DetailsUser = () => {
     setPassUsua(e.target.value);
   }
 
-  const handlePersbUserChange = (e) => {
+  const handleNombPersChange = (e) => {
     setNombPers(e.target.value);
   }
 
-  const handleSumitChange = (e) =>{
+  const handleAppaPersChange = (e) => {
+    setAppaPers(e.target.value);
+  }
+
+  const handleApmaPersChange = (e) => {
+    setApmaPers(e.target.value);
+  }
+
+  const handleDniPersChange = (e) => {
+    setDniPers(e.target.value);
+  }
+
+  const handleTelfPersChange = (e) => {
+    setTelfPers(e.target.value);
+  }
+  const handleEmailPersChange = (e) => {
+    setEmailPers(e.target.value);
+  }
+
+  const handleNombRolChange = (e) => {
+    setNombRol(e.target.value);
+  }
+
+  const handleSumitChange = async(e) =>{
+    e.preventDefault();
     
-    const user  =  AddUser(
+    const user  =  await AddUser(
       nombUsua,
       passUsua,
       nombPers,
@@ -45,7 +69,11 @@ export const DetailsUser = () => {
       nombRol
     );
 
-     //user.mensaje?console.log("Error al cargar") : "Dtos cargados"
+    if (user.message) {
+      console.log("error en grabado")
+      return;
+    }
+    navigate(`/DetailsUser`);
   }
 
   return (
@@ -56,17 +84,17 @@ export const DetailsUser = () => {
         {/* Main Content */}
         <div className="mainCard">
           <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
-            <form>
+            <form onSubmit={handleSumitChange}>
               {/* Form Default */}
               <div>
                 <label htmlFor="defaultInput" className="text-sm text-gray-600">
-                  Default Input
+                  Nombre Usuario
                 </label>
                 <input
                   id="defaultInput"
                   type="text"
                   name="defaultInput"
-                  // onChange={(e) => setEmail(e.target.value)}
+                  onChange={handleNombUserChange}
                   className="text-sm placeholder-gray-500 px-4 rounded-lg border border-gray-200 w-full md:py-2 py-3 focus:outline-none focus:border-emerald-400 mt-1"
                   placeholder="Default Input"
                 />
@@ -75,43 +103,21 @@ export const DetailsUser = () => {
               {/* Form Large */}
               <div className="mt-6">
                 <label htmlFor="largeInput" className="text-sm text-gray-600">
-                  Large Input
+                  Password
                 </label>
                 <input
                   id="largeInput"
                   type="text"
                   name="largeInput"
-                  // onChange={(e) => setEmail(e.target.value)}
+                  onChange={handlePassUserChange}
                   className="text-xl placeholder-gray-500 px-4 rounded-lg border border-gray-200 w-full md:py-2 py-3 focus:outline-none focus:border-emerald-400 mt-1"
                   placeholder="Large Input"
                 />
               </div>
 
-              {/* With Icon */}
-              <div className="mt-6 relative">
-                <label
-                  htmlFor="inputWithIcon"
-                  className="text-sm text-gray-600"
-                >
-                  Input with Icon
-                </label>
-
-                <div className="inline-flex items-center justify-center absolute left-0 top-[0.85rem] h-full w-10 text-gray-400">
-                  <FontAwesomeIcon icon={faPhone} />
-                </div>
-                <input
-                  id="inputWithIcon"
-                  type="text"
-                  name="inputWithIcon"
-                  // onChange={(e) => setEmail(e.target.value)}
-                  className="text-sm placeholder-gray-500 pl-10 px-4 rounded-lg border border-gray-200 w-full md:py-2 py-3 focus:outline-none focus:border-emerald-400 mt-1"
-                  placeholder="Input With Icon"
-                />
-              </div>
-
               <div className="mt-6 flex flex-row gap-4">
                 <button className="bg-emerald-600 text-gray-100 px-3 py-2 rounded-lg shadow-lg text-sm">
-                  Primary Button
+                  Confirmar
                 </button>
 
                 <button className="text-emerald-600 border border-gray-100 px-3 py-2 rounded-lg shadow-lg text-sm">
